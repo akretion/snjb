a096ba3fa6159a5e0c650852aa878eed5fd42faf
Florian da Costa <florian.dacosta@akretion.com>
2024-12-15 18:56:08 +0100

    [IMP] Always search product per supplier code

--- base_c/models/product.py
+++ base_c/models/product.py
@@ -1,5 +1,21 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.osv import expression
 
 
 class ProductProduct(models.Model):
     _inherit = "product.product"
+
+    @api.model
+    def _search_display_name(self, operator, value):
+        domain = super()._search_display_name(operator, value)
+        # we want to be able to search by supplier code for the product from any place
+        # (but mainly sale order). So, systematically search on seller_ids.product_code
+        # unless we got partner in context (in that case native Odoo already search
+        # for the product_code in combination with the supplier, this should be enough
+        if not self.env.context.get('partner_id'):
+            domains = [domain]
+            is_positive = operator not in expression.NEGATIVE_TERM_OPERATORS
+            combine = expression.OR if is_positive else expression.AND
+            domains.append([('product_tmpl_id.seller_ids.product_code', operator, value)])
+            domain = combine(domains)
+        return domain
