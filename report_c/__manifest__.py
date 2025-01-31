0a9e2404f2c39af4643336562d73116ede504b60
Florian da Costa <florian.dacosta@akretion.com>
2025-01-31 17:43:00 +0100

    [IMP] Hide some useless stuff on invoice report to make some room

--- report_c/__manifest__.py
+++ report_c/__manifest__.py
@@ -1,11 +1,12 @@
 {
     "name": "Reports Custom",
     "author": "Akretion",
-    "version": "18.0.1.0.0",
+    "version": "18.0.1.0.1",
     "license": "AGPL-3",
     "category": "Reporting",
     "depends": [
         "base_c",
+        "l10n_fr_account",
     ],
     "data": [
         "views/sale_rpt.xml",
