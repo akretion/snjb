dec740c61df77ba82e39fd84744b475b1a51a549
Florian da Costa <florian.dacosta@akretion.com>
2025-02-12 12:05:48 +0100

    [IMP] Make search by suppler default code work from product.product field instead of only product.template

--- base_c/models/product.py
+++ base_c/models/product.py
@@ -27,6 +27,22 @@ class ProductProduct(models.Model):
             domain = combine(domains)
         return domain
 
+    @api.model
+    def name_search(self, name='', args=None, operator='ilike', limit=100):
+        res = super().name_search(name=name, args=args, operator=operator, limit=limit)
+        # maybe we should do it even if a res exists in case we do not have any
+        # self.env.context.get("partner_id")
+        if not res:
+            domain = args or []
+            # still no results, partner in context: search on supplier info as last hope to find something
+            supplier_domain = [
+                ('product_code', operator, name),
+            ]
+            match_domain = [('product_tmpl_id.seller_ids', 'any', supplier_domain)]
+            products = self.search_fetch(expression.AND([domain, match_domain]), ['display_name'], limit=limit)
+            res = [(product.id, product.display_name) for product in products.sudo()] 
+        return res
+
 
 class ProductTemplate(models.Model):
     _inherit = "product.template"
